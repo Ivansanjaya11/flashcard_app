81b03cc51c6919c37815d986be7ead0468121362
Ivan Sanjaya <ivansanjaya11keluer@gmail.com>
2024-11-21 20:55:41 -0700

    test

---
+++ cmake-build-debug/CMakeFiles/flashcard_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flashcard_app.dir/main.cpp.obj"
+  "CMakeFiles/flashcard_app.dir/main.cpp.obj.d"
+  "flashcard_app.exe"
+  "flashcard_app.exe.manifest"
+  "flashcard_app.pdb"
+  "libflashcard_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flashcard_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
